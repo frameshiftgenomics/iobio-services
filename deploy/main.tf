09437f1103712f9b380c2d4204f09965ecb06a43
Jared Gollhardt <jared@nebula.org>
2020-04-16 10:39:45 -0700

    Remove instance profile, add HTTP access to instance

--- deploy/main.tf
+++ deploy/main.tf
@@ -15,64 +15,44 @@ provider "aws" {
 }
 
 resource "aws_instance" "iobio" {
-  ami                  = "ami-07ebfd5b3428b6f4d"
-  instance_type        = "m5.2xlarge"
-  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
-
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 150
-  }
+  // TODO Use data source to find lastest iobio-services ami in our account
+  ami             = "ami-07ebfd5b3428b6f4d"
+  instance_type   = "m5.2xlarge"
+  security_groups = [aws_security_group.allow_http.id]
 
   tags = {
     Name = "iobio-prod"
   }
 }
 
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP/HTTPS inbound traffic"
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_eip" "eip" {
   instance = aws_instance.iobio.id
-}
-
-data "aws_iam_policy_document" "instance-assume-role-policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "instance" {
-  name               = "AmazonS3FullAccessForEC2Iobio"
-  assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
-}
-
-data "aws_iam_policy_document" "s3_read_access" {
-  statement {
-    actions = [
-      "s3:*",
-    ]
-
-    resources = [
-      "arn:aws:s3:::nebula-iobio-services",
-      "arn:aws:s3:::nebula-iobio-services/*",
-    ]
-  }
-}
-
-resource "aws_iam_policy" "s3_read_access" {
-  name   = "S3ReadAccessIobio"
-  policy = data.aws_iam_policy_document.s3_read_access.json
-}
-
-resource "aws_iam_role_policy_attachment" "attach_role" {
-  role       = aws_iam_role.instance.name
-  policy_arn = aws_iam_policy.s3_read_access.arn
-}
-
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "AmazonS3FullAccessForEC2Iobio"
-  role = aws_iam_role.instance.name
 }
